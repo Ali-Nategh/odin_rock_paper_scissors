502652061575e8c3e4f9c9a0d8a7a31062e13b88
Ali-Nategh <alinategh005@gmail.com>
2022-07-28 14:32:54 +0430

    Added buttons and added eventListeners to them

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-rock-paper-scissors
 A rock paper scissors game to practice JavaScript
+
+Added UI with JS to the project
--- script.js
+++ script.js
@@ -6,35 +6,47 @@ function getComputerChoice() {
         return "paper";
     } else if (rand <= 99){
         return "scissors";
-    }
-}
+    };
+};
 
-function playRound(){
-    playerChoice = prompt("rock, paper or scissors?").toLowerCase()
+function playRound(choice){
+    playerChoice = choice;
     let computerChoice = getComputerChoice();
     if (playerChoice == computerChoice){
-        return `It's a tie! You both chose ${playerChoice}`
+        return `It's a tie! You both chose ${playerChoice}`;
     } else if (playerChoice == "rock" && computerChoice == "scissors"){
-        return "You won! Rock beats Scissors"
+        return "You won! Rock beats Scissors";
     } else if (computerChoice == "rock" && playerChoice == "scissors"){
-        return "You lost! Rock beats Scissors"
+        return "You lost! Rock beats Scissors";
     } else if (playerChoice == "paper" && computerChoice == "rock"){
-        return "You won! Paper beats Rock"
+        return "You won! Paper beats Rock";
     } else if (computerChoice == "paper" && playerChoice == "rock"){
-        return "You lost! Paper beats Rock"
+        return "You lost! Paper beats Rock";
     } else if (playerChoice == "scissors" && computerChoice == "paper"){
-        return "You won! Scissors beats Paper"
+        return "You won! Scissors beats Paper";
     } else if (computerChoice == "scissors" && playerChoice == "paper"){
-        return "You lost! Scissors beats Paper"
+        return "You lost! Scissors beats Paper";
     } else {
-        return `error! please enter a valid input: -> ${playRound()}`
-    }
-}
+        return `error! please enter a valid input: -> ${playRound()}`;
+    };
+};
 
 function game(){
-    for (let i = 0; i < 5; i++){
         console.log(playRound());
-    }
 }
 
-game()
+const btn_rock = document.querySelector('#btnRock');
+const btn_paper = document.querySelector('#btnPaper');
+const btn_scissors = document.querySelector('#btnScissors');
+
+btn_rock.addEventListener('click', () => {
+    console.log(playRound("rock"));
+});
+btn_paper.addEventListener('click', () => {
+    console.log(playRound("paper"));
+});
+btn_scissors.addEventListener('click', () => {
+    console.log(playRound("scissors"));
+});
+
+// game()
